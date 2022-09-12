4f740b784250b224d512a736fde8a9e4a9537f7e
Diego Dmitry <diegodmitry@hotmail.com>
2022-09-12 18:55:30 -0300

    feat(python): added req 3.

--- src/insights.py
+++ src/insights.py
@@ -3,7 +3,6 @@ from src.jobs import read
 
 def get_unique_job_types(path):
     jobs = read(path)
-    print('jobs', jobs)
     jobs_types_list = []
     for job in jobs:
         jobs_types_list.append(job["job_type"])
@@ -35,21 +34,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries = read(path)
+    industries_list = []
+    for industry in industries:
+        # a funcao desconsidera valores vazios
+        if industry["industry"] != '':
+            industries_list.append(industry["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return set(industries_list)
 
 
 def filter_by_industry(jobs, industry):
