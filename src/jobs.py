68ff196ce64ebae9cd0b19cacb0ab30d431720fc
Diego Dmitry <diegodmitry@hotmail.com>
2022-09-12 17:21:45 -0300

    feat(python): added req 1.

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,9 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, 'r') as csv_file:
+        list_of_dicts = list(csv.DictReader(csv_file))
+        return list_of_dicts
